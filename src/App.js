c8984e991ffde9305f1a5d8cf349bd40fe4844fd
Tran Thanh Xuan <ttxuan0595@gmail.com>
2021-06-07 20:27:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+
+import './App.css';
+import './homepage.scss';
+function App() {
+  return (
+    <div>
+      <div className="homepage">
+        <div className="homepage-row">
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">JACKETS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">SNEAKERS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <div className="homepage-row">
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">MAN</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">WOMAN</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
